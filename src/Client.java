b865b9237ef11a729ff5d12d840ab1f5ada4c541
Zach Essel <tuk01401@temple.edu>
2020-02-19 13:25:31 -0500

    Initial commit of skeleton

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+        System.out.printf("I am the client class");
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,8 @@
+
+public class Server {
+
+	public static void main(String[] args) {
+        System.out.printf("I am the server class");
+	}
+
+}
